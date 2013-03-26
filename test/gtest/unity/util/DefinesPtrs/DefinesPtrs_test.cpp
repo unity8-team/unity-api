c40c6121f2a58057fcf7afe2945e57cb10a41004
Michi Henning <michi.henning@canonical.com>
2013-03-27 06:18:59 +1000

    Added coverage for DefinesPtrs.

---
+++ test/gtest/unity/util/DefinesPtrs/DefinesPtrs_test.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2013 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Michi Henning <michi.henning@canonical.com>
+ */
+
+#include <unity/util/DefinesPtrs.h>
+#include <unity/util/NonCopyable.h>
+
+#include <gtest/gtest.h>
+
+using namespace unity::util;
+
+class MyClass : public DefinesPtrs<MyClass>, private NonCopyable
+{
+public:
+    static Ptr create()
+    {
+        return Ptr(new MyClass);
+    }
+
+private:
+    MyClass()
+    {
+    }
+};
+
+TEST(DefinesPtrs, basic)
+{
+    // No real test here. This is just so we get function coverage for the coverage tests.
+
+    MyClass::Ptr p = MyClass::create();
+}
