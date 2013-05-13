f9b34c0e8ee2c6533f331e7b1c162972c3e74872
Michał Sawicz <michal.sawicz@canonical.com>
2013-05-13 17:27:29 +0200

    add Notifications shell API headers

---
+++ include/unity/shell/notifications/notification.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2013 Canonical Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *      Michał Sawicz <michal.sawicz@canonical.com>
+ */
+
+
+#ifndef NOTIFICATION_H
+#define NOTIFICATION_H
+
+#include <unity/config.h>
+
+#include <QtCore/QObject>
+
+namespace unity {
+namespace shell {
+namespace notifications {
+
+class UNITY_API NotificationInterface : public QObject
+{
+    Q_OBJECT
+public:
+    explicit NotificationInterface(QObject* parent = 0) : QObject(parent) { }
+    virtual ~NotificationInterface() { }
+
+public Q_SLOTS:
+    virtual void onHovered() = 0;
+    virtual void onDisplayed() = 0;
+    virtual void invokeAction(const QString& id) = 0;
+
+Q_SIGNALS:
+    void dismissed();
+};
+
+} // namespace notifications
+} // namespace shell
+} // namespace unity
+
+#endif // NOTIFICATION_H
---
+++ include/unity/shell/notifications/source.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2013 Canonical Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *      Michał Sawicz <michal.sawicz@canonical.com>
+ */
+
+
+#ifndef SOURCE_H
+#define SOURCE_H
+
+#include <unity/config.h>
+
+#include <QtCore/QObject>
+
+namespace unity {
+namespace shell {
+namespace notifications {
+
+class ModelInterface;
+
+class UNITY_API SourceInterface : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(unity::shell::notifications::ModelInterface* model READ model WRITE setModel NOTIFY modelChanged)
+public:
+    explicit SourceInterface(QObject* parent = 0) : QObject(parent) { }
+    virtual ~SourceInterface() { }
+
+    virtual ModelInterface* model() const = 0;
+
+    virtual void setModel(ModelInterface* model) = 0;
+
+Q_SIGNALS:
+    void modelChanged(ModelInterface* model);
+};
+
+} // namespace notifications
+} // namespace shell
+} // namespace unity
+
+#endif // SOURCE_H
