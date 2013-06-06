683eceb3d61bde1ec1c27f4bb119bdd49269fcd6
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-06-06 15:02:05 +0300

    Added test fixture for IniParser.

---
+++ test/gtest/unity/util/IniParser/IniParser_test.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2013 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Jussi Pakkanen <jussi.pakkanen@canonical.com>
+ */
+
+#include <gtest/gtest.h>
+#include <unity/util/IniParser.h>
+#include <unity-api-test-config.h>
+
+using namespace std;
+using namespace unity;
+using namespace unity::util;
+
+TEST(IniParser, basic)
+{
+}
