894ce6e6c1b6fcef91721e75c2269db8bf16cb00
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-06-18 15:15:32 +0300

    Added precompiled header support for unity-api target.

---
+++ src/pch/unityapi_pch.hh
@@ -0,0 +1,16 @@
+/*
+ * List of (system) headers to precompile.
+ * This is not a fire-and-forget file, the list
+ * of headers must be updated as includes change.
+ *
+ * Grepping for includes in include and src
+ * will give a pretty good idea what to include.
+ */
+
+#include<memory>
+#include<vector>
+#include<map>
+#include<string>
+#include<mutex>
+#include<exception>
+#include<sstream>
