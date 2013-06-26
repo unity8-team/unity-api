97f3edcd354152821bfba17d9639af2708bf5acb
Michi Henning <michi.henning@canonical.com>
2013-06-27 09:47:34 +1000

    Changed "Lesser GNU General Public License" to "GNU Lesser General Public License" throughout. Fixed five files that used GPL instead of LGPL. licensecheck comes up clean now, reporting LGPL v3 for everything.

--- test/whitespace/check_whitespace.py
+++ test/whitespace/check_whitespace.py
@@ -4,15 +4,15 @@
 # Copyright (C) 2013 Canonical Ltd
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the Lesser GNU General Public License version 3 as
+# it under the terms of the GNU Lesser General Public License version 3 as
 # published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# Lesser GNU General Public License for more details.
+# GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the Lesser GNU General Public License
+# You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Authored by: Michi Henning <michi.henning@canonical.com>
