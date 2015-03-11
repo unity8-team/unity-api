131ed343389afe40d725663360d6623425db72a5
Michael Zanetti <michael.zanetti@canonical.com>
2015-03-11 18:16:47 +0100

    drop mir dep from api again

--- include/unity/shell/application/Globals.h
+++ include/unity/shell/application/Globals.h
@@ -14,11 +14,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef GLOBALS_H
-#define GLOBALS_H
+#ifndef UNITY_SHELL_APPLICATION_GLOBALS_H
+#define UNITY_SHELL_APPLICATION_GLOBALS_H
 
 #include <QObject>
-#include <mir_toolkit/common.h>
 
 namespace unity
 {
@@ -35,34 +34,34 @@ class Globals
 
 public:
     enum SurfaceType {
-        Normal = mir_surface_type_normal,
-        Utility = mir_surface_type_utility,
-        Dialog = mir_surface_type_dialog,
-        Gloss = mir_surface_type_gloss,
-        Freestyle = mir_surface_type_freestyle,
-        Menu = mir_surface_type_menu,
-        InputMethod = mir_surface_type_inputmethod,
-        Satellite = mir_surface_type_satellite,
-        Tip = mir_surface_type_tip
+        Normal,
+        Utility,
+        Dialog,
+        Gloss,
+        Freestyle,
+        Menu,
+        InputMethod,
+        Satellite,
+        Tip
     };
 
     enum SurfaceState {
-        Unknown = mir_surface_state_unknown,
-        Restored = mir_surface_state_restored,
-        Minimized = mir_surface_state_minimized,
-        Maximized = mir_surface_state_maximized,
-        VertMaximized = mir_surface_state_vertmaximized,
-        /* SemiMaximized = mir_surface_state_semimaximized, // see mircommon/mir_toolbox/common.h*/
-        Fullscreen = mir_surface_state_fullscreen,
+        Unknown,
+        Restored,
+        Minimized,
+        Maximized,
+        VertMaximized,
+        /* SemiMaximized, // see mircommon/mir_toolbox/common.h*/
+        Fullscreen
     };
 
     enum PixelFormat {
-        Invalid = mir_pixel_format_invalid,
-        ABGR8888 = mir_pixel_format_abgr_8888,
-        XBGR8888 = mir_pixel_format_xbgr_8888,
-        ARGB8888 = mir_pixel_format_argb_8888,
-        XRGB8888 = mir_pixel_format_xrgb_8888,
-        BGR888 = mir_pixel_format_bgr_888,
+        Invalid,
+        ABGR8888,
+        XBGR8888,
+        ARGB8888,
+        XRGB8888,
+        BGR888
     };
 
 private:
@@ -74,4 +73,4 @@ private:
 } // shell
 } // application
 
-#endif // GLOBALS_H
+#endif // UNITY_SHELL_APPLCATION_GLOBALS_H
