96dd3443a158ca2c9e7a27842ebea48651eb4a5f
Nick Dedekind <nick.dedekind@canonical.com>
2015-11-23 11:04:35 +0000

    setStage

--- include/unity/shell/application/ApplicationInfoInterface.h
+++ include/unity/shell/application/ApplicationInfoInterface.h
@@ -80,7 +80,7 @@ class UNITY_API ApplicationInfoInterface: public QObject
      *
      * Holds the stage where this application is currently located.
      */
-    Q_PROPERTY(Stage stage READ stage NOTIFY stageChanged)
+    Q_PROPERTY(Stage stage READ stage WRITE setStage NOTIFY stageChanged)
 
     /**
      * @brief The application's state.
@@ -262,6 +262,7 @@ public:
     virtual QString comment() const = 0;
     virtual QUrl icon() const = 0;
     virtual Stage stage() const = 0;
+    virtual void setStage(Stage) = 0;
     virtual State state() const = 0;
     virtual RequestedState requestedState() const = 0;
     virtual void setRequestedState(RequestedState) = 0;
